9a2bee57e00e990785614a9bde3ceeffd54a5638
Jacob Thompson <jacthomp@bb136-22.adit.mines.edu>
2017-06-13 15:33:58 -0600

    Nevermind fixed it

--- blitzlocker/about_dialog.py
+++ blitzlocker/about_dialog.py
@@ -3,7 +3,7 @@ from blitzlocker import Gtk
 def show_about_dialog(item):
     about = Gtk.AboutDialog()
     about.set_transient_for(None)
-    about.set_name('BlitzLocker')
+    about.set_program_name('BlitzLocker')
     about.set_version('0.0.1')
     about.set_comments("A password manager for SalesForce")
     about.set_authors(["Jack Rosenthal", "Charlie McConnell", "Jacob Thompson"])
