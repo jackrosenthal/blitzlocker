4d5d4a70a2150eac21f7b9aca03a3f0cbc311ae0
Jacob Thompson <jacthomp@bb136-22.adit.mines.edu>
2017-06-13 15:29:26 -0600

    The about dialog works, need to change the name of the main script from lickerapp to blitzlocker for it to display properly

--- blitzlocker/about_dialog.py
+++ blitzlocker/about_dialog.py
@@ -2,6 +2,7 @@ from blitzlocker import Gtk
 
 def show_about_dialog(item):
     about = Gtk.AboutDialog()
+    about.set_transient_for(None)
     about.set_name('BlitzLocker')
     about.set_version('0.0.1')
     about.set_comments("A password manager for SalesForce")
