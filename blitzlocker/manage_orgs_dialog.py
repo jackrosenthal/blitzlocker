7ca5212311bd5856ef3f9116cd9deb76d54594c6
Charles McConnell <cmcconne@bb136-23.adit.mines.edu>
2017-06-13 17:37:59 -0600

    various ui updates

--- blitzlocker/manage_orgs_dialog.py
+++ blitzlocker/manage_orgs_dialog.py
@@ -20,8 +20,9 @@ class ManageOrgsDialog(Gtk.Window):
         site_url_cr = Gtk.CellRendererText()
         self.site_combo_box.pack_start(site_url_cr,True)
         self.site_combo_box.add_attribute(site_url_cr,"text",0)
+        
         self.grid.add(self.site_combo_box)
-       
+        
         
         self.bbox = Gtk.ButtonBox()
         self.add_org_button = Gtk.Button(label='Add Org...')
@@ -35,11 +36,12 @@ class ManageOrgsDialog(Gtk.Window):
         self.add(self.grid)
 
     def open_add_org(self, widget):
-        asd = AddSiteDialog(liststore=self.liststore, transient_for=self, modal=True)
-        asd.present()
+        self.active_combo = self.site_combo_box.get_active_iter()
+        aod = AddOrgDialog(liststore=self.liststore, transient_for=self, modal=True)
+        aod.present()
 
 
-class AddSiteDialog(Gtk.Dialog):
+class AddOrgDialog(Gtk.Dialog):
     def __init__(self, liststore, *args, **kwargs):
         Gtk.Dialog.__init__(self, *args, title="Add Org", **kwargs)
         self.liststore = liststore
@@ -47,17 +49,27 @@ class AddSiteDialog(Gtk.Dialog):
 
         self.grid = Gtk.Grid()
 
+        self.site_label = Gtk.Label("Site: ")
+        self.treeview=Gtk.TreeView(self.liststore)
+        self.site_combo_box = Gtk.ComboBox.new_with_model(self.liststore)
+        site_url_cr = Gtk.CellRendererText()
+        self.site_combo_box.pack_start(site_url_cr,True)
+        self.site_combo_box.add_attribute(site_url_cr,"text",0)
+        
+        self.grid.attach(self.site_label,0,0,1,1)
+        self.grid.attach_next_to(self.site_combo_box,self.site_label,Gtk.PositionType.RIGHT,2,1)
+        
         self.user_textbox = Gtk.Entry()
         self.user_textbox.set_placeholder_text("Username ")
         self.user_textbox.set_activates_default(True)
-        self.user_label = (Gtk.Label("Username"))
+        self.user_label = Gtk.Label("Username: ")
 
-        self.grid.attach(self.user_label,0,0,1,1)
+        self.grid.attach_next_to(self.user_label,self.site_label,Gtk.PositionType.BOTTOM,1,1)
         self.grid.attach_next_to(self.user_textbox,self.user_label,Gtk.PositionType.RIGHT,2,1)
 
         self.pw_textbox = Gtk.Entry()
         self.pw_textbox.set_placeholder_text("********")
-        self.pw_label = Gtk.Label("Password ")
+        self.pw_label = Gtk.Label("Password: ")
         self.gen_pw_button = Gtk.Button(label = 'Generate')
         
         self.grid.attach_next_to(self.pw_label,self.user_label,Gtk.PositionType.BOTTOM,1,1)
@@ -65,15 +77,16 @@ class AddSiteDialog(Gtk.Dialog):
         self.grid.attach_next_to(self.gen_pw_button,self.pw_textbox,Gtk.PositionType.RIGHT,1,1)
 
         self.desc_textbox = Gtk.Entry()
-        self.desc_label = Gtk.Label("Description")
+        self.desc_label = Gtk.Label("Description: ")
         
         self.grid.attach_next_to(self.desc_label,self.pw_label,Gtk.PositionType.BOTTOM,1,1)
         self.grid.attach_next_to(self.desc_textbox,self.desc_label,Gtk.PositionType.RIGHT,2,1)
 
+        
         self.add_button("Cancel", 0)
-        asb = self.add_button("Add Org", 1)
-        asb.set_can_default(True)
-        asb.grab_default()
+        aob = self.add_button("Add Org", 1)
+        aob.set_can_default(True)
+        aob.grab_default()
 
         self.connect("response", self.response)
         self.get_content_area().add(self.grid)
