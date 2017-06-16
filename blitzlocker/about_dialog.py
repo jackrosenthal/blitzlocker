cbb5d9c151a5882e7057c9b346e0e1cea4970b43
Jack Rosenthal <jack@rosenth.al>
2017-06-16 16:47:18 -0600

    version bump

--- blitzlocker/about_dialog.py
+++ blitzlocker/about_dialog.py
@@ -6,7 +6,7 @@ def show_about_dialog(item):
     about = Gtk.AboutDialog()
     about.set_transient_for(None)
     about.set_program_name('BlitzLocker')
-    about.set_version('0.4.0')
+    about.set_version('Version 1.0.0 (Golden Master Release)')
     about.set_comments("A password manager for SalesForce")
     about.set_authors(["Jack Rosenthal", "Charlie McConnell", "Jacob Thompson"])
     about.set_logo(logo)
