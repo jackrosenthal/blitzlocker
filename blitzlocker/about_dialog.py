9b95de79fd51f68cd26e676647922b5c8df341a4
Jack Rosenthal <jack@rosenth.al>
2017-06-16 13:30:09 -0600

    add logo to about dialog

--- blitzlocker/about_dialog.py
+++ blitzlocker/about_dialog.py
@@ -1,4 +1,6 @@
-from blitzlocker import Gtk
+from blitzlocker import Gtk, GdkPixbuf
+
+logo = GdkPixbuf.Pixbuf.new_from_file_at_scale("res/blitz.png", 128, 128, True)
 
 def show_about_dialog(item):
     about = Gtk.AboutDialog()
@@ -7,5 +9,6 @@ def show_about_dialog(item):
     about.set_version('0.4.0')
     about.set_comments("A password manager for SalesForce")
     about.set_authors(["Jack Rosenthal", "Charlie McConnell", "Jacob Thompson"])
+    about.set_logo(logo)
     about.run()
     about.destroy()
