b161eb81e76bc0e4d69482853056c127463bda38
Jack Rosenthal <jack@rosenth.al>
2017-06-19 19:55:06 -0600

    use logo from pixbuf in about dialog

--- blitzlocker/systrayicon.py
+++ blitzlocker/systrayicon.py
@@ -1,11 +1,11 @@
 from blitzlocker import Gtk, basepath
+from blitzlocker.about_dialog import logo
 
 class TrayIcon(Gtk.StatusIcon):
     def __init__(self, menu):
         Gtk.StatusIcon.__init__(self)
         self.menu = menu
-
-        self.set_from_file(basepath + "/res/icon64.png")
+        self.set_from_pixbuf(logo)
         self.set_tooltip_text("BlitzLocker")
         self.connect("button-press-event", self.click_event)
 
